248cc6218db13cde24938e8b88412fb5c33fea29
Lnaa <ibnufires@gmail.com>
2024-12-20 01:25:15 +0700

    Initial Commit

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ document.getElementById("navbar").classList.remove("navbar-scroll");
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 56) {
     navEL.classList.add("navbar-scroll");
+    navEL.classList.add("navbar-light");
+    navEL.classList.remove("navbar-dark");
   } else {
     navEL.classList.remove("navbar-scroll");
+    navEL.classList.remove("navbar-light");
+    navEL.classList.add("navbar-dark");
   }
 });
